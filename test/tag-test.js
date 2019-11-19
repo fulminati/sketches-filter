185af5e64b659a37f79caf00ccb29a08bb2ae079
Francesco Bianco <info.francescobianco@gmail.com>
2019-11-20 00:38:25 +0100

    introducing test

--- test/tag-test.js
+++ test/tag-test.js
@@ -1,24 +1,20 @@
-"use strict";
+'use strict';
 
-var cli = require("../src/cli");
-var ops = require("../src/ops");
-var util = require("../src/util");
-var chai = require("chai");
+const chai = require('chai')
+    , join = require('path').join
+    , copy = require('fs-extra').copySync
+    , applyTag = require('..').applyTag
+    , resetTag = require('..').resetTag
 
-chai.use(require("chai-fs"));
+chai.use(require('chai-fs'))
 
-describe("Testing command-line interface", function () {
+describe('testing tag', function () {
 
-    it("dockerops", function () {
-        var exec = cli.run();
-        chai.assert.equal(exec, "docker-compose ps");
-        var exec = cli.run([]);
-        chai.assert.equal(exec, "docker-compose ps");
-    });
+    it('apply', function () {
+        copy(join(__dirname, 'fixtures'), 'temp')
+        applyTag('test', 'temp', (args) => {
+            return "test"
+        })
+    })
 
-    it("dockerops up", function () {
-        var exec = cli.run(["up"]);
-        chai.assert.equal(exec, "docker-compose up -d --remove-orphans");
-    });
-
-});
+})
